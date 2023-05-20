86fb7df450a06a266811af891a7ddb7492b00125
oskiefitz <115146064+oskiefitz@users.noreply.github.com>
2023-05-20 23:08:45 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -427,7 +427,7 @@ scene.background = new THREE.Color('black');
 
 {
   const objLoader = new OBJLoader();
-  objLoader.load('assets/bunny.obj', (root) => {
+  objLoader.load('assets/TARGET.obj', (root) => {
     bunny = root;
     scene.add(root);
   });
